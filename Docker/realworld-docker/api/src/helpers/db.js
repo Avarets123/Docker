8e83b78c844a4d7b5ded38ddcdff57ceb32e0a58
Osman <avarets1234@gmail.com>
2022-06-18 02:27:07 +0300

    edit

--- Docker/realworld-docker/api/src/helpers/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const { db } = require('../configuration')
-
-module.exports.connectDb = () => {
-    mongoose.connect(db);
-
-    return mongoose.connection;
-}
